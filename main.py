87ae17fc3b732d02adf8f5eb90bd7d4b10f666e9
ChoZinLMM <160577451+ChoZinLMM@users.noreply.github.com>
2024-03-21 06:55:06 +0000

    final submission

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
 
-# Type your code below
+stdform_list = list(stdform)
+valid = list("0123456789x.^eE-")
+caret_find = int(stdform.find('^'))
+exponent = stdform[caret_find + 1:]
+error = "Error converting to scientific E notation."
+
+for x in range(len(stdform)):
+  if stdform[x] not in valid:
+    print(error)
+    exit()
+
+if ' ' in stdform:
+  print(error)
+
+for sign in ['.', 'x', '^']:
+  if stdform_list.count(sign) > 1:
+    print(error)
+    exit()
+
+for x in range(len(exponent)):
+  if exponent[x] not in "0123456789-":
+    print(error)
+    exit()
+
+else :
+  notation = stdform.replace('x10^', 'E')
+  print('This number in E notation is '+ notation + '.')
